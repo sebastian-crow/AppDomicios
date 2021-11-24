b6d451c611d003862d1c732af7ed6868f238f563
stev vallejo <stevenvallejo780@gmail.com>
2021-11-24 00:15:46 -0500

    feat service worker

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/scss/paper-dashboard.scss?v=1.3.0';
 import 'assets/demo/demo.css';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
-
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { store, history } from './store/configureStore';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,3 +21,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
+
+serviceWorkerRegistration.register();
+reportWebVitals();
