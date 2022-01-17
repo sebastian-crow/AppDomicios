d1927d938739d90714f2e0f971a898f5d482e8b2
sebastian-crow <sebastianberiio@gmail.com>
2022-01-16 23:37:52 -0500

    Modal UserProductOrderList

--- src/components/addresses/orders/productOrders/UserProductOrderList.jsx
+++ src/components/addresses/orders/productOrders/UserProductOrderList.jsx
@@ -6,7 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { push } from 'redux-first-history';
 
 // React Bootstrap
-import { Button } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap';
 
 // Reducers
 import {
@@ -140,23 +146,42 @@ const UserProductOrderList = () => {
                     Eliminar
                   </Button>
                 </td>
-                {activeLink ? (
-                  <td>{link}</td>
-                ) : (
-                  <td>
-                    <Button
-                      onClick={(e) =>
-                        handleSetLikn(
-                          e,
-                          (`${process.env.REACT_APP_REACT_HOST}/client/takeorder/${user.id}/${order['Numero de Orden']}/${order['Nombres y Apellidos']}`)
-                        )
-                      }
-                      variant="success"
-                    >
-                      Crear Link{' '}
-                    </Button>
-                  </td>
-                )}
+                <td>
+                  <Button
+                    color="danger"
+                    onClick={() => alert('hasjdsk')}
+                  >
+                    Crear Link
+                  </Button>
+                  <Modal toggle={function noRefCheck() {}}>
+                    <ModalHeader toggle={function noRefCheck() {}}>
+                      Modal title
+                    </ModalHeader>
+                    <ModalBody>
+                      Lorem ipsum dolor sit amet, consectetur
+                      adipisicing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum
+                      dolore eu fugiat nulla pariatur. Excepteur sint
+                      occaecat cupidatat non proident, sunt in culpa
+                      qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        color="primary"
+                        onClick={function noRefCheck() {}}
+                      >
+                        Do Something
+                      </Button>{' '}
+                      <Button onClick={function noRefCheck() {}}>
+                        Cancel
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
+                </td>
               </tr>
             ))}
           </tbody>
